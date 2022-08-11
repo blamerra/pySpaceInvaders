b620a54c79a246d7064278b39a64f111d845d0df
IDB\rblanco <rblanco@iadb.org>
2022-08-11 10:55:35 -0400

    movimiento player

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import pygame
+# Constants
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+PLAYER_SPEED = 0.3
+PLAYER_SIZE = 64
+
 # test git IDB Laptop
 # Initialize game
 pygame.init()
@@ -6,25 +12,40 @@ icon = pygame.image.load("media/img/ico.png")
 pygame.display.set_icon(icon)
 pygame.display.set_caption("Space Invaders")
 pygame.event.set_allowed([pygame.QUIT, pygame.KEYDOWN, pygame.KEYUP])
-screen = pygame.display.set_mode((800, 600))
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+playerImg = pygame.image.load("media/img/spaceship.png")
+playerX = SCREEN_WIDTH/2 - PLAYER_SIZE/2
+playerY = SCREEN_HEIGHT - PLAYER_SIZE - SCREEN_HEIGHT * 0.1
+playerChange = 0
+
+def player(x, y):
+    screen.blit(playerImg, (x, y))
+
 
 # Game loop
 running = 1
 while running:
+
     # Event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = 0
 
+        # TODO Controlar evento key up si se pulsa la otra direccion muy rapido
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT or pygame.K_LEFT:
+                playerChange = 0
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerChange = -PLAYER_SPEED
+            if event.key == pygame.K_RIGHT:
+                playerChange = PLAYER_SPEED
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # Render Screen
+    screen.fill((255, 255, 255))
+    playerX += playerChange
+    player(playerX, playerY)
+    pygame.display.update()
