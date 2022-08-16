a0dcc5181a2d10207f1b62414e8ebecb47c4261c
raul <blamerra@gmail.com>
2022-08-16 12:20:44 -0400

    game class refactor

--- fps.py
+++ fps.py
@@ -10,15 +10,11 @@ class Fps(pygame.sprite.Sprite):
         self.image = pygame.Surface((20, 20))
         self.rect = self.image.get_rect()
         self.rect.center = (self.settings.screen_width - 10, 10)
-        self.clock = pygame.time.Clock()
         self.font = pygame.font.SysFont("Arial", 18)
         self.fps = None
         self.fps_text = None
 
     def update(self):
         self.image.fill(pygame.Color("black"))
-
-        self.clock.tick(self.settings.fps)
-        self.fps = str(int(self.clock.get_fps()))
         self.fps_text = self.font.render(self.fps, True, pygame.Color("coral"))
         self.image.blit(self.fps_text, [0, 0])
--- game.py
+++ game.py
@@ -18,6 +18,8 @@ class Game:
         pygame.display.set_caption(self.settings.screen_title)
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
 
+        self.clock = pygame.time.Clock()
+
         # Sprites creation
         self.player = Player()
         self.background = Background()
@@ -25,25 +27,37 @@ class Game:
         self.sprites = pygame.sprite.Group()
         self.sprites.add(self.background, self.player, self.fps)
 
+        self.running = 1
+        self.pause = 0
+
+    def process_events(self):
+        # Event loop
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = 0
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == self.settings.shortcut_pause:
+                    self.pause = not self.pause
+                if event.key == self.settings.shortcut_background_scroll_pause:
+                    self.background.pause()
+
+    def run_logic(self):
+        pass
+
+    def render_screen(self):
+        # Render Screen
+        if not self.pause:
+            self.clock.tick(self.settings.fps)
+            self.fps.fps = str(int(self.clock.get_fps()))
+
+            self.sprites.update()
+            self.sprites.draw(self.screen)
+            pygame.display.update()
+
     def run(self):
-        running = 1
-        pause = 0
-        while running:
-
-            # Event loop
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    running = 0
-
-                if event.type == pygame.KEYDOWN:
-                    if event.key == self.settings.shortcut_pause:
-                        pause = not pause
-                    if event.key == self.settings.shortcut_background_scroll_pause:
-                        self.background.pause()
-
-            # Render Screen
-            if not pause:
-                self.sprites.update()
-                self.sprites.draw(self.screen)
-                pygame.display.update()
+        while self.running:
+            self.process_events()
+            self.run_logic()
+            self.render_screen()
         pygame.quit()
