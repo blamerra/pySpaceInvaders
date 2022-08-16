64d46a06c3f2620fbdb87bcce5937668fe6c72b3
raul <blamerra@gmail.com>
2022-08-16 18:33:07 -0400

    files cleanup

--- game.py
+++ game.py
@@ -51,6 +51,7 @@ class Game:
                     self.background.pause()
                 if event.key == self.settings.shortcut_fire:
                     self.bullet.fire(self.player.rect.x, self.player.rect.y)
+
     def run_logic(self):
         pass
 
